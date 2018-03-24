80c32b4ed2e3d9b7ee4032362d94cb8aa4616b70
Yadkee <yadkee@gmail.com>
2018-03-24 09:55:17 +0100

    Improved challege #352 [intermediate]

--- problems/[2018-02-21] Challenge #352 [Intermediate] 7 Wonders Resource Allocation.py
+++ problems/[2018-02-21] Challenge #352 [Intermediate] 7 Wonders Resource Allocation.py
@@ -1,6 +1,7 @@
 #! python3
 # -*- coding: utf-8 -*-
 # https://www.reddit.com/r/dailyprogrammer/comments/7z8hrm/20180221_challenge_352_intermediate_7_wonders/
+"""Inspired by u/Loonter's sorting idea"""
 from time import time
 
 
@@ -14,13 +15,17 @@ def ssolve(scards, starget):
             yield i
 
     def tuple_remove(t, o):
+        """Like a list.remove() but returns a new instance"""
         return tuple(yield_tuple_remove(iter(t), o))
 
     def relevant_cards(cards, targetSet):
+        """Remove cards that are not in target"""
+        targetSet = set(target)
         return tuple(i & targetSet for i in cards)
 
-    def can_win(cards, target, targetSet):
-        for i in targetSet:
+    def can_win(cards, target):
+        """Only can win if there are enough cards for target's needs"""
+        for i in set(target):
             tCount = target.count(i)
             cCount = sum(1 for j in cards if i in j)
             if tCount > cCount:
@@ -28,29 +33,39 @@ def ssolve(scards, starget):
         return True
 
     def solve(cards, target):
-        if len(target) == 1:
-            return target[0] in cards[0]
-        targetSet = set(target)
-        cards = relevant_cards(cards, targetSet)
-        chosen = min(cards)
-        if len(chosen) == 1:
+        if len(target) == 1:  # One card for one target
+            return target[0] in cards[0]  # They match
+        cards = relevant_cards(cards, target)
+        # We first go for cards with only one resource
+        for chosen in cards:
+            if len(chosen) != 1:
+                continue
             cards = tuple_remove(cards, chosen)
             target = tuple_remove(target, chosen.pop())
             return solve(cards, target)
-        if not can_win(cards, target, targetSet):
+        if not can_win(cards, target):
             return False
-        chosen = max(cards)
-        cards = tuple_remove(cards, chosen)
-        for i in chosen:
-            sTarget = tuple_remove(target, i)
-            s = solve(cards, sTarget)
+        # Remember that target is sorted by resource frequency in cards
+        chosen = target[0]
+        target = target[1:]
+        # We try the cards that contain this resource
+        for i in cards:
+            if chosen not in i:
+                continue
+            sCards = tuple_remove(cards, i)
+            s = solve(sCards, target)
             if s:
                 return True
+        # Nothing worked so there is no solution
         return False
     cards = tuple(set(i.split("/")) for i in scards.split(", "))
-    target = tuple(starget)
+
+    def letter_frecuency(l):
+        return sum(1 for j in cards if l in j)
+    target = tuple(sorted(starget, key=letter_frecuency))
     t0 = time()
     solution = solve(cards, target)
+    # Remember that True == 1 and False == 0
     print("It can%sbe solved. Took %.4f seconds" %
           ((" not ", " ")[solution], time() - t0))
 
@@ -59,8 +74,6 @@ ssolve("W/B/S/O, S/O, W/S, W/B, W/B, W, B", "WWBSSOO")
 ssolve("A/B/D/E, A/B/E/F/G, A/D, A/D/E, A/D/E, B/C/D/G, B/C/E, B/C/E/F, "
        "B/C/E/F, B/D/E, B/D/E, B/E/F, C/D/F, C/E, C/E/F/G, C/F, C/F, "
        "D/E/F/G, D/F, E/G", "AABCCCCCCDDDEEEEFFGG")
-raise SystemExit
-# 4th one can not be solved with this bruteforce solution in normal times
 ssolve("A/C/G/K/L/O/R/S, A/D/H/I/M/Q, A/D/K/W/X, A/D/M/U/Z, A/E/J/M/T, "
        "A/G/H/I/M/R/T/Z, A/G/M/T/U, A/H/I/J/Q, B/C/Q/U/V, B/D/F/K/M/R/W/Y, "
        "B/F/P/T/U/W/Y, B/G/K/M/S/T/X/Y, C/E/F/I/K/N/O, D/E/G/J/M/Q/Z, "
